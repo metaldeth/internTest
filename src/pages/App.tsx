897e3952c94ffd45d6f068e6b5b726802f877b9a
drapkinamaria <drapkinamaria02@gmail.com>
2024-06-16 15:17:40 +0500

    add extra-big.css and blurred elements

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,12 +2,16 @@ import {advantagesList, reviewsList} from "../components/constants";
 import "../css/big.css"
 import "../css/small.css"
 import "../css/medium.css"
+import "../css/extra-big.css"
 import {renderStars} from "../components/render-stars";
 
 function App() {
 
     return (
         <div className="godmanai">
+            <div className="first-blur-element"></div>
+            <div className="second-blur-element"></div>
+            <div className="centered-blur-box"></div>
             <div className="rise-with-ai-powered-sales-parent">
                 <b className="rise-with-ai-powered">Rise with AI-powered Sales</b>
                 <div className="welcome-to-the">
@@ -22,7 +26,7 @@ function App() {
                     {advantagesList.map((advantage) => (
                         <div className="feature" key={advantage.id}>
                             <div className="title">
-                                <svg width="21" height="21" viewBox="0 0 21 21" fill="#5ec6fa" xmlns="http://www.w3.org/2000/svg">
+                                <svg className="svg-size" width="21" height="21" viewBox="0 0 21 21" fill="#5ec6fa" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M2.88558 6.73021C-0.365196 9.98101 -0.365196 15.2516 2.88558 18.5024C6.13637 21.7532 11.4069 21.7532 14.6577 18.5024C17.9085 15.2516 23.1405 4.74892 19.8898 1.49812C16.639 -1.75268 6.13637 3.4794 2.88558 6.73021Z" fill="#5EC6FA" />
                                 </svg>
                                 <b className="fast-learning">{advantage.label}</b>
